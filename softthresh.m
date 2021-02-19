277ffac734235df9495775772b85ac46d0eb3580
MaxENSTAB <58601219+MaxENSTAB@users.noreply.github.com>
2021-02-19 17:57:04 +0100

    Après station de travail

--- softthresh.m
+++ softthresh.m
@@ -3,14 +3,17 @@ function [out] = softthresh(array,thresh)
 %   Detailed explanation goes here
 out = zeros(size(array));
 for i = 1:length(array)
-    if abs(array(i))<=thresh
-        out(i) = 0;
-    end
-    if array(i)>thresh
-        out(i) = array(i)-thresh;
-    end
-    if array(i)>thresh
-        out(i) = array(i)+thresh;
-    end
+%     if abs(array(i))<=thresh
+%         out(i) = 0;
+%     end
+%     if array(i)>thresh
+%         out(i) = array(i)-thresh;
+%     end
+%     if array(i)>thresh
+%         out(i) = array(i)+thresh;
+%     end
+
+out(i) = array(i)*(max(abs(array(i)-thresh),0))/((max(abs(array(i)-thresh),0)+thresh));
 end
+
 
